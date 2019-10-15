011160831b28cb6f74c6d2a69157829b268f7d76
Sanjoy <sanjoy637r@gmail.com>
2019-10-15 11:55:27 +0600

    vehicle

---
+++ Day15/Vehicle/Assignment10/Assignment10/MODEL/Vehicle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment10.MODEL
+{
+    public class Vehicle
+    {
+        public string Name { set; get; }
+        public string RegNo { set; get; }
+        public List<double> Speeds = new List<double>();
+
+        public Vehicle()
+        {
+
+        }
+
+        public Vehicle(string name, string regNo)
+        {
+            this.Name = name;
+            this.RegNo = regNo;
+        }
+
+        public void SetSpeed(double speed)
+        {
+            Speeds.Add(speed);
+        }
+
+        public double MinSpeed()
+        {
+            return Speeds.Min();
+        }
+        public double MaxSpeed()
+        {
+            return Speeds.Max();
+        }
+        public double AvgSpeed()
+        {
+            return Speeds.Average();
+        }
+    }
+}
---
+++ Day15/Vehicle/Assignment10/Assignment10/VehicleForm.cs
@@ -0,0 +1,49 @@
+﻿using Assignment10.MODEL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment10
+{
+    public partial class VehicleForm : Form
+    {
+        Vehicle _vehicle = new Vehicle();
+        //List<double> _speeds = new List<double>();
+        public VehicleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            _vehicle = new Vehicle(vehicleNameTextBox.Text, regNoTextBox.Text);
+
+            vehicleNameTextBox.Clear();
+            regNoTextBox.Clear();
+
+            MessageBox.Show("Vehicle " + _vehicle.Name + ", Reg No: " + _vehicle.RegNo + " created");
+            createButton.Enabled = false;
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            _vehicle.SetSpeed(double.Parse(speedTextBox.Text));
+            speedTextBox.Clear();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            minSpeedTextBox.Text = _vehicle.MinSpeed()+"";
+            maxSpeedTextBox.Text = _vehicle.MaxSpeed() + "";
+            averageSpeedTextBox.Text = _vehicle.AvgSpeed() + "";
+        }
+
+        
+    }
+}
